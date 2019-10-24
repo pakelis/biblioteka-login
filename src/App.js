365969924293394938deeca1b92ae2a8e86690e3
unknown <pakelis47@gmail.com>
2019-10-24 12:12:30 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import React from 'react'
 import Login from './components/Login'
 import Signup from './components/Signup'
 import Home from './components/Home'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
 import {AuthProvider} from './Auth'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import PrivateRoute from './Routes/PrivateRoute'
+
+//MATERIAL IMPORTS
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core'
+
+const theme = createMuiTheme({})
 
 function App() {
   return (
     //AuthProvider everything below it, will have acess to currentUser through the context API
     //In our case if we logged in we will have user Object with all the description, and if we log out we have null or undefined
-    <AuthProvider>
-      <Router>
-        <div>
-          <PrivateRoute exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-        </div>
-      </Router>
-    </AuthProvider>
+    <MuiThemeProvider theme={theme}>
+      <AuthProvider>
+        <Router>
+          <div>
+            <PrivateRoute exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+          </div>
+        </Router>
+      </AuthProvider>
+    </MuiThemeProvider>
   )
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,9 +2,45 @@ import React, {useCallback, useContext} from 'react'
 import {withRouter, Redirect} from 'react-router'
 import app from '../firebase'
 import {AuthContext} from '../Auth'
+import {
+  Button,
+  makeStyles,
+  TextField,
+  Typography,
+  Link,
+  Container,
+  CssBaseline,
+  FormControlLabel,
+  Checkbox,
+} from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}))
 
 const Login = ({history}) => {
-  //history prop we get useing withRouter
+  //history prop we get using withRouter
   const handleLogin = useCallback(
     async event => {
       event.preventDefault()
@@ -20,6 +56,7 @@ const Login = ({history}) => {
     [history],
   )
 
+  const classes = useStyles()
   const {currentUser} = useContext(AuthContext)
 
   if (currentUser) {
@@ -27,21 +64,51 @@ const Login = ({history}) => {
   }
 
   return (
-    <div>
-      <h1>Log in</h1>
-      <form onSubmit={handleLogin}>
-        <label>
-          email
-          <input name="email" type="email" placeholder="Email" />
-        </label>
-        <br />
-        <label>
-          password
-          <input name="password" type="password" placeholder="Password" />
-        </label>
-        <button type="submit">Log in</button>
-      </form>
-    </div>
+    <Container maxWidth="sm">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h3">
+          Log in
+        </Typography>
+        <form onSubmit={handleLogin} className={classes.form}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+        </form>
+      </div>
+    </Container>
   )
 }
 
