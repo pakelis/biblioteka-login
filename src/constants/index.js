53b3de6ad124cde112ff4958d933412ea790f906
unknown <pakelis47@gmail.com>
2019-11-17 13:33:41 +0200

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const collatedTasks = [
+  { key: "INBOX", name: "Inbox" },
+  { key: "INBOX", name: "Inbox" },
+  { key: "INBOX", name: "Inbox" }
+];
+//Collated tasks means = inbox, today ,next 7 days etc...
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import firebase from "../firebase";
+import moment from "moment";
 
 function getWindowsDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
@@ -26,7 +27,7 @@ export function useWindowDimensions() {
   return windowDimension;
 }
 
-const collatedTasks = () => {};
+const collatedTasksExist = () => {};
 
 export const useTasks = selectedRecord => {
   const [tasks, setTasks] = useState([]);
@@ -35,11 +36,11 @@ export const useTasks = selectedRecord => {
     let unsubscribe = firebase
       .firestore()
       .collection("tasks")
-      .where("userId", "==", "abc001");
+      .where("userId", "==", "abc001"); //Go get our tasks where userId is this
 
-    unsubscribe =
+    unsubscribe = //this one means give me the tasks for selected project, it can be 1 or TODAY or INBOX etc....
       selectedRecord && !collatedTasksExist(selectedRecord)
-        ? (unsubscribe = unsubscribe.where("projectId", "==", selectedRecord))
+        ? (unsubscribe = unsubscribe.where("recordId", "==", selectedRecord))
         : selectedRecord === "TODAY"
         ? (unsubscribe = unsubscribe.where(
             "date",
