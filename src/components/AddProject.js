b7a3cfbb7fb487c6035f7001b2c5e9b26f9ce1c5
unknown <pakelis47@gmail.com>
2019-12-08 23:39:07 +0200

    initial commit

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -1,38 +1,74 @@
-import React, {useState} from 'react'
-import {firebase} from '../firebase'
-import {generatePushId} from '../helpers'
-import {useProjectsValue} from '../context'
-import {Typography, Button} from '@material-ui/core'
+import React, { useState } from "react";
+import { firebase } from "../firebase";
+import { generatePushId } from "../helpers";
+import { useProjectsValue } from "../context";
+//Material
+import { makeStyles } from "@material-ui/core/styles";
+import Popover from "@material-ui/core/Popover";
+import { Typography, Button, Box, TextField } from "@material-ui/core";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 
-export const AddProject = ({shouldShow = false}) => {
-  const [show, setShow] = useState(shouldShow)
-  const [projectName, setProjectName] = useState('')
+const useStyles = makeStyles(theme => ({
+  addProject: {
+    display: "flex",
+    margin: "auto",
+    color: theme.palette.primary.main,
+    backgroundColor: "#fff"
+  },
+  icon: {
+    marginRight: "10px"
+  }
+}));
 
-  const projectId = generatePushId()
-  const {setProjects} = useProjectsValue()
+export const AddProject = ({ shouldShow = false }) => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [show, setShow] = useState(shouldShow);
+  const [projectName, setProjectName] = useState("");
+
+  const projectId = generatePushId();
+  const { setProjects } = useProjectsValue();
+
+  const handleClick = event => {
+    // setting anchor element position for popover to pop
+    setAnchorEl(event.currentTarget);
+    setShow(!show);
+  };
 
   const addProject = () => {
     projectName &&
       firebase
         .firestore()
-        .collection('projects')
+        .collection("projects")
         .add({
           projectId,
           name: projectName,
-          userId: 'abc001',
+          userId: "abc001"
         })
         .then(() => {
-          setProjects([])
-          setProjectName('')
-          setShow(false)
-        })
-  }
+          setProjects([]);
+          setProjectName("");
+          setShow(false);
+        });
+  };
 
   return (
     <div>
+      <Button fullWidth className={classes.addProject} onClick={handleClick}>
+        <AddCircleOutlineIcon className={classes.icon} />
+        Add Project
+      </Button>
       {show && (
-        <div>
-          <input
+        <Popover
+          open={show}
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left"
+          }}
+          className={classes.popover}
+        >
+          <TextField
             value={projectName}
             onChange={e => setProjectName(e.target.value)}
             type="text"
@@ -40,10 +76,8 @@ export const AddProject = ({shouldShow = false}) => {
           />
           <Button onClick={() => addProject()}>Add Project</Button>
           <Button onClick={() => setShow(false)}>Cancel</Button>
-        </div>
+        </Popover>
       )}
-      <Typography>+</Typography>
-      <Typography onClick={() => setShow(!show)}>Add Project</Typography>
     </div>
-  )
-}
+  );
+};
--- src/components/layout/NavItems.js
+++ src/components/layout/NavItems.js
@@ -1,54 +1,56 @@
-import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
-import {withStyles, makeStyles} from '@material-ui/core/styles'
-import MenuList from '@material-ui/core/List'
-import MenuItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
-import Divider from '@material-ui/core/Divider'
-import ListSubheader from '@material-ui/core/ListSubheader'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import Collapse from '@material-ui/core/Collapse'
-import InboxIcon from '@material-ui/icons/MoveToInbox'
-import DraftsIcon from '@material-ui/icons/Drafts'
-import SendIcon from '@material-ui/icons/Send'
-import ExpandLess from '@material-ui/icons/ExpandLess'
-import ExpandMore from '@material-ui/icons/ExpandMore'
-import StarBorder from '@material-ui/icons/StarBorder'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import MenuList from "@material-ui/core/List";
+import MenuItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Collapse from "@material-ui/core/Collapse";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import SendIcon from "@material-ui/icons/Send";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import StarBorder from "@material-ui/icons/StarBorder";
 //Icons
-import HomeIcon from '@material-ui/icons/Home'
-import DescriptionIcon from '@material-ui/icons/Description'
-import SaveIcon from '@material-ui/icons/Save'
-import BookmarksIcon from '@material-ui/icons/Bookmarks'
-import DeleteIcon from '@material-ui/icons/Delete'
-import NotesIcon from '@material-ui/icons/Notes'
-import {useSelectedProjectValue} from '../../context'
-import {width} from '@material-ui/system'
-import {Records} from '../Records'
+import HomeIcon from "@material-ui/icons/Home";
+import DescriptionIcon from "@material-ui/icons/Description";
+import SaveIcon from "@material-ui/icons/Save";
+import BookmarksIcon from "@material-ui/icons/Bookmarks";
+import DeleteIcon from "@material-ui/icons/Delete";
+import NotesIcon from "@material-ui/icons/Notes";
+import { useSelectedProjectValue } from "../../context";
+import { width } from "@material-ui/system";
+import { Records } from "../Records";
+
+//TODO make every menu item selected when i click on it (make it bold or smth)
 
 const useStyles = makeStyles(theme => ({
   root: {
-    marginTop: theme.spacing(8),
+    marginTop: theme.spacing(8)
   },
   listItem: {
-    paddingRight: theme.spacing(8),
+    paddingRight: theme.spacing(8)
   },
   active: {
-    fontWeight: '',
-  },
-}))
+    fontWeight: ""
+  }
+}));
 
 export const NavItems = () => {
-  const classes = useStyles()
-  const [open, setOpen] = useState(false)
-  const {setSelectedProject} = useSelectedProjectValue()
-  const [active, setActive] = useState('inbox')
-  const [showProjects, setShowProjects] = useState(true)
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const { setSelectedProject } = useSelectedProjectValue();
+  const [active, setActive] = useState("inbox");
+  const [showProjects, setShowProjects] = useState(true);
 
   const handleClick = () => {
-    setOpen(!open)
-  }
+    setOpen(!open);
+  };
 
   return (
     <div className={classes.root}>
@@ -58,46 +60,46 @@ export const NavItems = () => {
           button
           className={classes.listItem}
           onClick={() => {
-            setActive('inbox')
-            setSelectedProject('INBOX')
+            setActive("inbox");
+            setSelectedProject("INBOX");
           }}
         >
           <ListItemIcon>
             <HomeIcon />
           </ListItemIcon>
-          <ListItemText primary={'Inbox'} />
+          <ListItemText primary={"Inbox"} />
         </MenuItem>
         <MenuItem
           button
           className={classes.listItem}
           onClick={() => {
-            setActive('today')
-            setSelectedProject('TODAY')
+            setActive("today");
+            setSelectedProject("TODAY");
           }}
         >
           <ListItemIcon>
             <DescriptionIcon />
           </ListItemIcon>
-          <ListItemText primary={'Today'} />
+          <ListItemText primary={"Today"} />
         </MenuItem>
         <MenuItem
           button
           className={classes.listItem}
           onClick={() => {
-            setActive('next 7')
-            setSelectedProject('NEXT_7')
+            setActive("next 7");
+            setSelectedProject("NEXT_7");
           }}
         >
           <ListItemIcon>
             <SaveIcon />
           </ListItemIcon>
-          <ListItemText primary={'Next 7 days'} />
+          <ListItemText primary={"Next 7 days"} />
         </MenuItem>
         <MenuItem button onClick={handleClick} className={classes.listItem}>
           <ListItemIcon>
             <BookmarksIcon />
           </ListItemIcon>
-          <ListItemText primary={'Records'} />
+          <ListItemText primary={"Records"} />
           {open ? <ExpandLess /> : <ExpandMore />}
         </MenuItem>
         <Collapse in={open} timeout="auto" unmountOnExit>
@@ -122,5 +124,5 @@ export const NavItems = () => {
         </MenuItem>
       </MenuList> */}
     </div>
-  )
-}
+  );
+};
