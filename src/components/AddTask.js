c4bff1e1348db775f6660166424ab0f5d5a98b3a
pakelis <pakelis47@gmail.com>
2019-12-16 12:18:24 +0200

    initial commit

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,98 +1,129 @@
-import React, { useState } from "react";
-import moment from "moment";
-import { firebase } from "../firebase";
-import { useSelectedProjectValue } from "../context";
-import { Typography, TextField, Button, IconButton } from "@material-ui/core";
+import React, {useState} from 'react'
+import moment from 'moment'
+import {firebase} from '../firebase'
+import {useSelectedProjectValue} from '../context'
+import {Typography, TextField, Button, IconButton} from '@material-ui/core'
 //Material
-import { makeStyles } from "@material-ui/core";
-import AddIcon from "@material-ui/icons/Add";
-import { Box } from "@material-ui/core";
-import DateRangeIcon from "@material-ui/icons/DateRange";
-import EventNoteIcon from "@material-ui/icons/EventNote";
-import { ProjectOverlay } from "./ProjectOverlay";
-import { TaskDate } from "./TaskDate";
+import {makeStyles} from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add'
+import {Box} from '@material-ui/core'
+import DateRangeIcon from '@material-ui/icons/DateRange'
+import EventNoteIcon from '@material-ui/icons/EventNote'
+import {ProjectOverlay} from './ProjectOverlay'
+import {TaskDate} from './TaskDate'
+import Modal from '@material-ui/core/Modal'
+import Backdrop from '@material-ui/core/Backdrop'
+import Fade from '@material-ui/core/Fade'
 
 //TODO make textfield outline focused color different, more like lightAccent
 
 const useStyles = makeStyles(theme => ({
   root: {},
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.lightShade.main,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    width: '27%',
+  },
   textField: {
-    width: "100%"
+    width: '100%',
   },
   addIcon: {
-    marginRight: "5px"
+    marginRight: '5px',
   },
   addTask: {
     color: theme.palette.darkGrey.main,
-    display: "flex",
-    alignItems: "center",
+    display: 'flex',
+    alignItems: 'center',
     fontWeight: 500,
-    "&:hover": {
+    '&:hover': {
       color: theme.palette.lightAccent.main,
-      cursor: "pointer"
-    }
+      cursor: 'pointer',
+    },
   },
   addTaskContainer: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    marginTop: "10px"
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginTop: '10px',
   },
   iconsContainer: {
-    marginLeft: "auto",
-    display: "flex"
+    marginLeft: 'auto',
+    display: 'flex',
   },
   cancel: {
-    marginLeft: "15px",
-    fontWeight: "500",
-    "&:hover": {
-      textDecoration: "underline",
-      cursor: "pointer"
-    }
+    marginLeft: '15px',
+    fontWeight: '500',
+    '&:hover': {
+      textDecoration: 'underline',
+      cursor: 'pointer',
+    },
   },
   addTaskButton: {
-    color: "white",
+    color: 'white',
     backgroundColor: theme.palette.lightAccent.main,
-    "&:hover": {
-      backgroundColor: theme.palette.darkAccent.main
-    }
-  }
-}));
+    '&:hover': {
+      backgroundColor: theme.palette.darkAccent.main,
+    },
+  },
+}))
 
 export const AddTask = ({
   showAddTaskMain = true,
   shouldShowMain = false,
   showQuickAddTask,
-  setShowQuickAddTask
+  setShowQuickAddTask,
+  showText = true,
+  showModal = false,
 }) => {
-  const [task, setTask] = useState("");
-  const [taskDate, setTaskDate] = useState("");
-  const [project, setProject] = useState("");
-  const [showMain, setShowMain] = useState(shouldShowMain);
-  const [showProjectOverlay, setShowProjectOverlay] = useState(false);
-  const [showTaskDate, setShowTaskDate] = useState(false);
-  const [anchorEl, setAnchorEl] = useState();
+  const [task, setTask] = useState('')
+  const [taskDate, setTaskDate] = useState('')
+  const [project, setProject] = useState('')
+  const [showMain, setShowMain] = useState(shouldShowMain)
+  const [showProjectOverlay, setShowProjectOverlay] = useState(false)
+  const [showTaskDate, setShowTaskDate] = useState(false)
+  const [anchorEl, setAnchorEl] = useState()
+  const [openModal, setOpenModal] = useState(showModal)
 
-  const { selectedProject } = useSelectedProjectValue();
+  const {selectedProject} = useSelectedProjectValue()
 
-  const classes = useStyles();
+  const classes = useStyles()
 
   const handleClick = event => {
-    setAnchorEl(event.currentTarget);
-  };
+    setAnchorEl(event.currentTarget)
+  }
 
-  console.log(`Selected Project - ${selectedProject}`);
+  const handleModalOpen = () => {
+    setOpenModal(true)
+  }
+
+  const handleModalClose = () => {
+    setOpenModal(false)
+    setTimeout(() => {
+      setShowMain(false)
+      setShowProjectOverlay(false)
+      setShowQuickAddTask(false)
+      console.log('timeoutfunction')
+    }, 500)
+  }
+
+  console.log(`Selected Project - ${selectedProject}`)
 
   const addTask = () => {
-    const projectId = project || selectedProject;
-    let collatedDate = "";
+    const projectId = project || selectedProject
+    let collatedDate = ''
 
-    if (projectId === "TODAY") {
-      collatedDate = moment().format("DD/MM/YYYY");
-    } else if (projectId === "NEXT_7") {
+    if (projectId === 'TODAY') {
+      collatedDate = moment().format('DD/MM/YYYY')
+    } else if (projectId === 'NEXT_7') {
       collatedDate = moment()
-        .add(7, "days")
-        .format("DD/MM/YYYY");
+        .add(7, 'days')
+        .format('DD/MM/YYYY')
     }
 
     return (
@@ -100,52 +131,37 @@ export const AddTask = ({
       projectId &&
       firebase
         .firestore()
-        .collection("tasks")
+        .collection('tasks')
         .add({
-          archived: "false",
+          archived: 'false',
           projectId,
           task,
           date: collatedDate || taskDate,
-          userId: "abc001"
+          userId: 'abc001',
         })
         .then(() => {
-          setTask("");
-          setProject("");
-          setShowMain("");
-          setShowProjectOverlay(false);
+          setTask('')
+          setProject('')
+          setShowMain('')
+          setShowProjectOverlay(false)
         })
-    );
-  };
+    )
+  }
 
   return (
-    <Box style={{ marginTop: "15px" }}>
+    <Box style={{marginTop: '15px'}}>
       {showAddTaskMain && (
         <Box onClick={() => setShowMain(!showMain)}>
-          <Typography className={classes.addTask}>
-            <AddIcon className={classes.addIcon} /> Add Task
-          </Typography>
+          {showText && (
+            <Typography className={classes.addTask}>
+              <AddIcon className={classes.addIcon} /> Add Task
+            </Typography>
+          )}
         </Box>
       )}
       {/* Timestamp 3.35, can't understand why this opens AddTask component, need to make modal */}
-      {(showMain || showQuickAddTask) && (
-        <div style={{ marginTop: "15px" }}>
-          {showQuickAddTask && (
-            <>
-              <div>
-                <Typography variant="h2">Quick add task</Typography>
-                <Typography
-                  variant="body1"
-                  onClick={() => {
-                    setShowMain(false);
-                    setShowProjectOverlay(false);
-                    setShowQuickAddTask(false);
-                  }}
-                >
-                  X
-                </Typography>
-              </div>
-            </>
-          )}
+      {showMain && (
+        <div style={{marginTop: '15px'}}>
           <TaskDate
             setTaskDate={setTaskDate}
             showTaskDate={showTaskDate}
@@ -168,8 +184,8 @@ export const AddTask = ({
                 className={classes.cancel}
                 variant="body1"
                 onClick={() => {
-                  setShowMain(false);
-                  setShowProjectOverlay(false);
+                  setShowMain(false)
+                  setShowProjectOverlay(false)
                 }}
               >
                 Cancel
@@ -202,6 +218,97 @@ export const AddTask = ({
           />
         </div>
       )}
+      {showQuickAddTask && showModal && (
+        <div>
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            className={classes.modal}
+            open={openModal}
+            onClose={handleModalClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+            <Fade in={openModal}>
+              <div className={classes.paper}>
+                <div>
+                  <Typography variant="h6">Quick add task</Typography>
+                  <Typography
+                    variant="body1"
+                    onClick={() => {
+                      setShowMain(false)
+                      setShowProjectOverlay(false)
+                      setShowQuickAddTask(false)
+                    }}
+                  >
+                    X
+                  </Typography>
+                </div>
+                <TaskDate
+                  setTaskDate={setTaskDate}
+                  showTaskDate={showTaskDate}
+                  setShowTaskDate={setShowTaskDate}
+                  anchorEl={anchorEl}
+                />
+                <TextField
+                  placeholder="Enter task name"
+                  className={classes.textField}
+                  variant="outlined"
+                  value={task}
+                  onChange={e => setTask(e.target.value)}
+                />
+                <div className={classes.addTaskContainer}>
+                  <Button
+                    className={classes.addTaskButton}
+                    onClick={() => addTask()}
+                  >
+                    Add Task
+                  </Button>
+                  {!showQuickAddTask && (
+                    <Typography
+                      className={classes.cancel}
+                      variant="body1"
+                      onClick={() => {
+                        setShowMain(false)
+                        setShowProjectOverlay(false)
+                      }}
+                    >
+                      Cancel
+                    </Typography>
+                  )}
+                  <div className={classes.iconsContainer}>
+                    <Typography
+                      variant="body1"
+                      onClick={() => setShowProjectOverlay(!showProjectOverlay)}
+                    >
+                      <IconButton onClick={handleClick}>
+                        <EventNoteIcon color="action" />
+                      </IconButton>
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      onClick={() => setShowTaskDate(!showTaskDate)}
+                    >
+                      <IconButton onClick={handleClick}>
+                        <DateRangeIcon color="action" />
+                      </IconButton>
+                    </Typography>
+                  </div>
+                </div>
+                <ProjectOverlay
+                  setProject={setProject}
+                  showProjectOverlay={showProjectOverlay}
+                  setShowProjectOverlay={setShowProjectOverlay}
+                  anchorEl={anchorEl}
+                />
+              </div>
+            </Fade>
+          </Modal>
+        </div>
+      )}
     </Box>
-  );
-};
+  )
+}
--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { firebase } from "../../firebase";
-import ClassNames from "classnames";
-import { sizing } from "@material-ui/system";
-import { Switch, BrowserRouter, Route } from "react-router-dom";
+import React, {useState, useEffect} from 'react'
+import {firebase} from '../../firebase'
+import ClassNames from 'classnames'
+import {sizing} from '@material-ui/system'
+import {Switch, BrowserRouter, Route} from 'react-router-dom'
 //Material
-import AddOutlinedIcon from "@material-ui/icons/AddOutlined";
-import { makeStyles } from "@material-ui/core/styles";
-import MenuIcon from "@material-ui/icons/Menu";
+import AddOutlinedIcon from '@material-ui/icons/AddOutlined'
+import {makeStyles} from '@material-ui/core/styles'
+import MenuIcon from '@material-ui/icons/Menu'
 import {
   IconButton,
   Button,
@@ -14,81 +14,81 @@ import {
   Toolbar,
   Typography,
   Drawer,
-  Container
-} from "@material-ui/core";
-import { NavItems } from "./NavItems";
+  Container,
+} from '@material-ui/core'
+import {NavItems} from './NavItems'
 // import { Home } from "../Home";
 //hooks
-import { useWindowDimensions } from "../../hooks";
-import { Records } from "../Records";
-import { Tasks } from "../Tasks";
-import { AddProject } from "../AddProject";
-import { useSelectedProjectValue } from "../../context";
-import { AddTask } from "../AddTask";
+import {useWindowDimensions} from '../../hooks'
+import {Records} from '../Records'
+import {Tasks} from '../Tasks'
+import {AddProject} from '../AddProject'
+import {useSelectedProjectValue} from '../../context'
+import {AddTask} from '../AddTask'
 
 const useStyles = makeStyles(theme => ({
-  "@global": {
+  '@global': {
     body: {
-      backgroundColor: theme.palette.lightShade.main
-    }
+      backgroundColor: theme.palette.lightShade.main,
+    },
   },
   // The main flex container for the app's layout. Its min-height
   // is set to `100vh` so it always fill the height of the screen.
   root: {
-    display: "flex",
-    minHeight: "100vh",
+    display: 'flex',
+    minHeight: '100vh',
     zIndex: 1,
-    position: "relative",
-    overflow: "hidden",
-    backgroundColor: theme.palette.lightShade.main
+    position: 'relative',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.lightShade.main,
   },
   menuButton: {
-    marginRight: theme.spacing(2)
+    marginRight: theme.spacing(2),
   },
   menuName: {
     // flexGrow: 1 //take up all the space
   },
   appBar: {
-    zIndex: theme.zIndex.drawer + 1
+    zIndex: theme.zIndex.drawer + 1,
   },
   toolbarActions: {
-    marginLeft: "auto"
+    marginLeft: 'auto',
   },
   appContent: theme.mixins.gutters({
-    flex: "1 1 100%",
-    maxWidth: "60%",
+    flex: '1 1 100%',
+    maxWidth: '60%',
     paddingTop: 80, // equal to AppBar height + 16px
-    margin: "0 auto",
-    alignItems: "center",
-    marginLeft: "270px",
+    margin: '0 auto',
+    alignItems: 'center',
+    marginLeft: '270px',
     // Set the max content width for each breakpoint
     // Content will be centered in the space to the right/left of drawer
-    [theme.breakpoints.up("sm")]: {
-      maxWidth: theme.breakpoints.values.sm
-    }
+    [theme.breakpoints.up('sm')]: {
+      maxWidth: theme.breakpoints.values.sm,
+    },
   }),
   addIcon: {
-    color: "white"
-  }
-}));
+    color: 'white',
+  },
+}))
 
 const Content = props => {
-  const [open, setOpen] = useState(true);
-  const classes = useStyles();
-  const { width, height } = useWindowDimensions();
-  const { selectedProject } = useSelectedProjectValue();
-  const [shouldShowMain, setShouldShowMain] = useState(false);
-  const [showQuickAddTask, setShowQuickAddTask] = useState(false);
+  const [open, setOpen] = useState(true)
+  const classes = useStyles()
+  const {width, height} = useWindowDimensions()
+  const {selectedProject} = useSelectedProjectValue()
+  const [shouldShowMain, setShouldShowMain] = useState(false)
+  const [showQuickAddTask, setShowQuickAddTask] = useState(false)
 
   const handleDrawer = () => {
-    setOpen(!open);
-  };
+    setOpen(!open)
+  }
 
   //if screen get to certain width it closes and vice versa
   useEffect(() => {
-    width < 860 && open && setOpen(false);
-    width > 860 && !open && setOpen(true);
-  }, [width]);
+    width < 860 && open && setOpen(false)
+    width > 860 && !open && setOpen(true)
+  }, [width])
 
   return (
     <div className={classes.root}>
@@ -104,24 +104,24 @@ const Content = props => {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.menuName}>
-            {selectedProject === "TODAY"
-              ? "Today"
-              : selectedProject === "INBOX"
-              ? "Inbox"
-              : selectedProject === "NEXT_7"
-              ? "Next 7 days"
-              : "Records"}
+            {selectedProject === 'TODAY'
+              ? 'Today'
+              : selectedProject === 'INBOX'
+              ? 'Inbox'
+              : selectedProject === 'NEXT_7'
+              ? 'Next 7 days'
+              : 'Records'}
           </Typography>
           <IconButton
             onClick={() => {
-              setShowQuickAddTask(true);
-              setShouldShowMain(true);
+              setShowQuickAddTask(true)
+              setShouldShowMain(true)
             }}
           >
             <AddOutlinedIcon className={classes.addIcon} />
           </IconButton>
           <Button
-            style={{ marginLeft: "auto" }}
+            style={{marginLeft: 'auto'}}
             onClick={() => firebase.auth().signOut()}
             //button has onClick handler that will sign us out using firebase API
             color="inherit"
@@ -131,12 +131,6 @@ const Content = props => {
           </Button>
         </Toolbar>
       </AppBar>
-      <AddTask
-        showAddTaskMain={false}
-        shouldShowMain={shouldShowMain}
-        showQuickAddTask={showQuickAddTask}
-        showAddTaskMain={setShowQuickAddTask}
-      />
       <BrowserRouter>
         <Drawer variant="persistent" anchor="left" open={open}>
           <NavItems />
@@ -149,10 +143,18 @@ const Content = props => {
             <Route path="/tasks" component={Tasks} />
           </Switch>
           <Tasks />
+          <AddTask
+            showAddTaskMain={false}
+            shouldShowMain={shouldShowMain}
+            showQuickAddTask={showQuickAddTask}
+            setShowQuickAddTask={setShowQuickAddTask}
+            showText={false}
+            showModal={true}
+          />
         </main>
       </BrowserRouter>
     </div>
-  );
-};
+  )
+}
 
-export default Content;
+export default Content
