850cb8206049c3152f7880af53a13c2977639d29
unknown <pakelis47@gmail.com>
2019-12-23 00:12:09 +0200

    initial commit

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,78 +1,93 @@
-import React, {useState, useEffect} from 'react'
-import {Typography, Checkbox} from '@material-ui/core'
-import {useTasks} from '../hooks'
-import {collatedTasks} from '../constants'
-import {getTitle, getCollatedTitle, collatedTasksExist} from '../helpers'
-import {useSelectedProjectValue, useProjectsValue} from '../context'
-import {CheckBox} from './CheckBox'
-import {firebase} from '../firebase'
+import React, { useState, useEffect } from "react";
+import { Typography, Checkbox } from "@material-ui/core";
+import { useTasks } from "../hooks";
+import { collatedTasks } from "../constants";
+import { getTitle, getCollatedTitle, collatedTasksExist } from "../helpers";
+import { useSelectedProjectValue, useProjectsValue } from "../context";
+import { CheckBox } from "./CheckBox";
+import { firebase } from "../firebase";
+import { useUserValue } from "../Auth";
+import Moment from "moment";
 //Material
-import SortByAlphaOutlinedIcon from '@material-ui/icons/SortByAlphaOutlined'
-import Divider from '@material-ui/core/Divider'
-import {makeStyles} from '@material-ui/core/styles'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
-import IconButton from '@material-ui/core/IconButton'
-import {AddTask} from './AddTask'
+import SortByAlphaOutlinedIcon from "@material-ui/icons/SortByAlphaOutlined";
+import UpdateIcon from "@material-ui/icons/Update";
+import Divider from "@material-ui/core/Divider";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import IconButton from "@material-ui/core/IconButton";
+import { AddTask } from "./AddTask";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    width: '95%',
-    maxWidth: 720,
+    width: "95%",
+    maxWidth: 720
   },
   title: {
-    padding: '15px',
+    padding: "15px"
   },
   tasksHeader: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-}))
+    display: "flex",
+    alignItems: "center"
+  }
+}));
 
 //timestamp 1:12
 
 //Timestamp 3:45
 
 export const Tasks = () => {
-  const classes = useStyles()
-  const {selectedProject} = useSelectedProjectValue()
-  const {projects} = useProjectsValue()
-  const {tasks} = useTasks(selectedProject) // gets all the tasks from our useTasks hook in /hooks
+  const classes = useStyles();
+  const { selectedProject } = useSelectedProjectValue();
+  const { projects } = useProjectsValue();
+  const { tasks } = useTasks(selectedProject); // gets all the tasks from our useTasks hook in /hooks
+  const [sortedTasks, setSortedTasks] = useState([]);
 
   // console.log(`selectedProject - ${selectedProject}`);
   // console.log(`tasks - ${tasks}`);
 
-  const sortByAlpha = () => {}
+  const sortByAlpha = tasks => {
+    let sorted = tasks.sort((a, b) => a.task.localeCompare(b.task));
+    setSortedTasks(sorted);
+    tasks = sortedTasks;
+  };
+
+  const sortByDate = tasks => {
+    let sorted = tasks.sort((a, b) => a.date - b.date);
+    setSortedTasks(sorted);
+    tasks = sortedTasks;
+    console.log(sorted);
+  };
 
   const archiveTask = id => {
     firebase
       .firestore()
-      .collection('tasks')
+      .collection("tasks")
       .doc(id)
       .update({
-        archived: true,
-      })
-  }
+        archived: true
+      });
+  };
 
-  let projectName = ''
+  let projectName = "";
 
   if (
     projects.length > 0 &&
     selectedProject &&
     !collatedTasksExist(selectedProject)
   ) {
-    projectName = getTitle(projects, selectedProject).name
+    projectName = getTitle(projects, selectedProject).name;
   }
 
   if (collatedTasksExist(selectedProject) && selectedProject) {
-    projectName = getCollatedTitle(collatedTasks, selectedProject).name
+    projectName = getCollatedTitle(collatedTasks, selectedProject).name;
   }
 
   useEffect(() => {
-    document.title = `${projectName}: Todoist`
-  })
+    document.title = `${projectName}: Todoist`;
+  });
 
   return (
     <List className={classes.root}>
@@ -80,9 +95,12 @@ export const Tasks = () => {
         <Typography variant="h4" className={classes.title}>
           {projectName}
         </Typography>
-        <div style={{marginLeft: 'auto'}}>
-          <IconButton>
+        <div style={{ marginLeft: "auto" }}>
+          <IconButton onClick={() => sortByAlpha(tasks)}>
             <SortByAlphaOutlinedIcon />
+          </IconButton>
+          <IconButton onClick={() => sortByDate(tasks)}>
+            <UpdateIcon />
           </IconButton>
         </div>
       </div>
@@ -103,5 +121,5 @@ export const Tasks = () => {
       ))}
       <AddTask />
     </List>
-  )
-}
+  );
+};
