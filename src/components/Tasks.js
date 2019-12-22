d058733dfd63bf7df0101970fc0376009c52aafa
pakelis <pakelis47@gmail.com>
2019-12-22 21:02:41 +0200

    initial commit

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,69 +1,69 @@
-import React, { useState, useEffect } from "react";
-import { Typography, Checkbox } from "@material-ui/core";
-import { useTasks } from "../hooks";
-import { collatedTasks } from "../constants";
-import { getTitle, getCollatedTitle, collatedTasksExist } from "../helpers";
-import { useSelectedProjectValue, useProjectsValue } from "../context";
-import { CheckBox } from "./CheckBox";
-import { firebase } from "../firebase";
+import React, {useState, useEffect} from 'react'
+import {Typography, Checkbox} from '@material-ui/core'
+import {useTasks} from '../hooks'
+import {collatedTasks} from '../constants'
+import {getTitle, getCollatedTitle, collatedTasksExist} from '../helpers'
+import {useSelectedProjectValue, useProjectsValue} from '../context'
+import {CheckBox} from './CheckBox'
+import {firebase} from '../firebase'
 //Material
-import { makeStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import { AddTask } from "./AddTask";
+import {makeStyles} from '@material-ui/core/styles'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import {AddTask} from './AddTask'
 
 const useStyles = makeStyles(theme => ({
   root: {
-    width: "100%",
-    maxWidth: 460
+    width: '100%',
+    maxWidth: 460,
   },
   title: {
-    padding: "15px"
-  }
-}));
+    padding: '15px',
+  },
+}))
 
 //timestamp 1:12
 
 //Timestamp 3:45
 
 export const Tasks = () => {
-  const classes = useStyles();
-  const { selectedProject } = useSelectedProjectValue();
-  const { projects } = useProjectsValue();
-  const { tasks } = useTasks(selectedProject); // gets all the tasks from our useTasks hook in /hooks
+  const classes = useStyles()
+  const {selectedProject} = useSelectedProjectValue()
+  const {projects} = useProjectsValue()
+  const {tasks} = useTasks(selectedProject) // gets all the tasks from our useTasks hook in /hooks
 
-  console.log(`selectedProject - ${selectedProject}`);
-  console.log(`tasks - ${tasks}`);
+  // console.log(`selectedProject - ${selectedProject}`);
+  // console.log(`tasks - ${tasks}`);
 
   const archiveTask = id => {
     firebase
       .firestore()
-      .collection("tasks")
+      .collection('tasks')
       .doc(id)
       .update({
-        archived: true
-      });
-  };
+        archived: true,
+      })
+  }
 
-  let projectName = "";
+  let projectName = ''
 
   if (
     projects.length > 0 &&
     selectedProject &&
     !collatedTasksExist(selectedProject)
   ) {
-    projectName = getTitle(projects, selectedProject).name;
+    projectName = getTitle(projects, selectedProject).name
   }
 
   if (collatedTasksExist(selectedProject) && selectedProject) {
-    projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+    projectName = getCollatedTitle(collatedTasks, selectedProject).name
   }
 
   useEffect(() => {
-    document.title = `${projectName}: Todoist`;
-  });
+    document.title = `${projectName}: Todoist`
+  })
 
   return (
     <List className={classes.root}>
@@ -84,5 +84,5 @@ export const Tasks = () => {
       ))}
       <AddTask />
     </List>
-  );
-};
+  )
+}
