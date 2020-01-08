c4ce65310a41a519907fac3efd8bd07caa1b9f12
pakelis <pakelis47@gmail.com>
2020-01-08 23:43:00 +0200

    initial commit

--- src/components/AddProjectColorGrid.js
+++ src/components/AddProjectColorGrid.js
@@ -1,71 +1,74 @@
-import React, { useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, {useState, useEffect} from 'react'
+import {makeStyles} from '@material-ui/core/styles'
+import CheckIcon from '@material-ui/icons/Check'
 
 const useStyles = makeStyles(theme => ({
   container: {
-    display: "grid",
-    gridColumnGap: "6px",
-    gridRowGap: "5px",
-    paddingTop: "15px",
-    paddingBottom: "15px",
-    justifyContent: "center"
+    display: 'grid',
+    gridColumnGap: '6px',
+    gridRowGap: '5px',
+    paddingTop: '15px',
+    paddingBottom: '15px',
+    justifyContent: 'center',
   },
   bubble: {
-    color: "red",
-    height: "35px",
-    width: "35px",
-    borderRadius: "50%"
-  }
-}));
+    color: 'red',
+    height: '35px',
+    width: '35px',
+    borderRadius: '50%',
+  },
+}))
 
-let colorIndex = 0;
-const numRows = 3;
-const numCols = 5;
+let colorIndex = 0
+const numRows = 3
+const numCols = 5
 const colors = [
-  "blueviolet",
-  "khaki",
-  "crimson",
-  "dimgray",
-  "olivedrab",
-  "violet",
-  "gold",
-  "salmon",
-  "darkorange",
-  "olive",
-  "teal",
-  "purple",
-  "indigo",
-  "hotpink",
-  "saddlebrown",
-  "sandybrown"
-];
+  'blueviolet',
+  'khaki',
+  'crimson',
+  'dimgray',
+  'olivedrab',
+  'violet',
+  'gold',
+  'salmon',
+  'darkorange',
+  'olive',
+  'teal',
+  'purple',
+  'indigo',
+  'hotpink',
+  'saddlebrown',
+  'sandybrown',
+]
 
 export const AddProjectColorGrid = () => {
-  const classes = useStyles();
+  const classes = useStyles()
   //Making grid of bubbles
   const [grid, setGrid] = useState(() => {
-    const rows = [];
+    const rows = []
     for (let i = 0; i < numRows; i++) {
-      rows.push(Array.from(Array(numCols), () => 0));
+      rows.push(Array.from(Array(numCols), () => 0))
     }
 
-    return rows;
-  });
+    return rows
+  })
   const [selectedColor, setSelectedColor] = useState(() => {
-    let item = grid[Math.floor(Math.random() * grid.length * 5)];
-    return item;
-  });
+    let item = grid[Math.floor(Math.random() * grid.length * 5)]
+    return item
+  })
 
-  const colorPick = () => {};
+  const selectedBubble = () => {}
 
   useEffect(() => {
-    colorIndex = 0; // whenever component mounts we change colorIndex to 0 so we get all the colors
-  }, []);
+    // we change colorIndex , so we always get the same colors
+    colorIndex = 0
+  })
+  // we don't pass second parameter so it's same as componentDidMount & componentDidUpdate
 
   return (
     <div
       className={classes.container}
-      style={{ gridTemplateColumns: `repeat(${numCols}, 35px)` }} // Using css grid to make grid of bubbles
+      style={{gridTemplateColumns: `repeat(${numCols}, 35px)`}} // Using css grid to make grid of bubbles
     >
       {grid.map((rows, i) =>
         rows.map((cols, k) => {
@@ -73,11 +76,26 @@ export const AddProjectColorGrid = () => {
             <div
               key={`${i}-${k}`}
               className={classes.bubble}
-              style={{ backgroundColor: colors[colorIndex++] }}
+              style={{
+                backgroundColor: colors[colorIndex++],
+                border: grid[i][k] ? 'solid 1px black' : undefined,
+              }}
+              onClick={() => {
+                setGrid(prevGrid =>
+                  prevGrid.map((gridRow, index) =>
+                    index === i
+                      ? gridRow.map((gridCol, kIndex) => {
+                          console.log(gridCol)
+                          return kIndex === k ? 1 : gridCol
+                        })
+                      : gridRow,
+                  ),
+                )
+              }}
             ></div>
-          );
-        })
+          )
+        }),
       )}
     </div>
-  );
-};
+  )
+}
